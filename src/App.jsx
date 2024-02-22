d0b00e6c6913fb81beec7a10742ea318460c67a6
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-23 04:50:55 +0600

    initial the project with react, tailwind, firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+
+
+const App = () => {
+  return (
+    <div>
+    <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+    return (
+        <div>
+           <h3>This is Main</h3> 
+        </div>
+    );
+};
+
+export default Main;
